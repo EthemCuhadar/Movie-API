ec2e007b987c1168fb996a47e911a5fa00bbfbe7
EthemCuhadar <iethemcuhadar@gmail.com>
2022-03-30 10:25:25 +0300

    Connected to local database server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"log"
 
-	utils "github.com/EthemCuhadar/Movie-API/pkg/helper"
+	database "github.com/EthemCuhadar/Movie-API/pkg/migrations"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	MovieList, _ := utils.ReadJSON("C://Users/iethe/go/src/Movie-API/movies.json")
-	fmt.Println(reflect.TypeOf(MovieList[99]))
+	// MovieList, _ := utils.ReadJSON("C://Users/iethe/go/src/Movie-API/movies.json")
+	// fmt.Println(reflect.TypeOf(MovieList[99]))
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.NewPsqlDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if db == nil {
+		fmt.Println("db is nil")
+	}
 }
