568583774e5e072ad53445fdf755c39506f967fd
EthemCuhadar <iethemcuhadar@gmail.com>
2022-03-30 09:08:34 +0300

    main function reorganized

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
 
+	utils "github.com/EthemCuhadar/Movie-API/pkg/helper"
+)
+
+func main() {
+	MovieList, _ := utils.ReadJSON("C://Users/iethe/go/src/Movie-API/movies.json")
+	fmt.Println(reflect.TypeOf(MovieList[99]))
 }
